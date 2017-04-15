be01f30b6a2b1f75d9b3227494995d88f13462d3
Diego Rincon <drincon@scirestudios.com>
2017-04-15 16:47:33 -0500

    Project setup

---
+++ Podfile
@@ -0,0 +1,8 @@
+
+platform :ios, '10.3'
+
+target 'UIStates' do
+  use_frameworks!
+  pod 'SnapKit', '~> 3.1.2'
+
+end
---
+++ Pods/Target Support Files/Pods-UIStates/Pods-UIStates-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UIStates : NSObject
+@end
+@implementation PodsDummy_Pods_UIStates
+@end
